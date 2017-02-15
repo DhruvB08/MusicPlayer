6b177a223e85e2bd4c3f67e303e0f4a6c9faa692
IlyaSamoylov45 <ilyasamoylov45@gmail.com>
2017-02-14 22:46:14 -0500

    Update SongLib.java

--- SongLib.java
+++ SongLib.java
@@ -1,42 +1,33 @@
-package SongLib;
-
-import java.io.IOException;
-
-import SongLib.view.Controller;
+package app;
+	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import view.Controller;
+
 
 public class SongLib extends Application {
+	@Override
+	public void start(Stage primaryStage) 
+	throws Exception {
+		FXMLLoader loader = new FXMLLoader();   
+	      loader.setLocation(
+	         getClass().getResource("/view/Interface.fxml"));
+	      AnchorPane root = (AnchorPane)loader.load();
 
-	Stage mainStage;
-	
-	public void start(Stage stage) {
-		mainStage = stage;
-		mainStage.setTitle("Music Player");
-	
-		try {
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(getClass().getResource("view/Interface.fxml"));
-			AnchorPane pane = (AnchorPane)loader.load();
-			
-			Controller controller = loader.getController();
-			controller.setMainStage(mainStage);
-			
-			Scene scene = new Scene(pane, 500, 500);
-			mainStage.setResizable(false);
+	      Controller listController = 
+	         loader.getController();
+	      listController.start(primaryStage);
 
-			mainStage.setScene(scene);
-			mainStage.show();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+	      Scene scene = new Scene(root, 500, 500);
+	      primaryStage.setResizable(false);
+	     
+	      primaryStage.setScene(scene);
+	      primaryStage.show(); 
+
 	}
-	
 	
 	public static void main(String[] args) {
 		launch(args);
